60781b5614038dd10eaf51717d8323bac84162fd
bhojani <sh827646@dal.ca>
2022-04-09 17:40:44 -0300

    deleting changes

--- src/main/java/Authentication/Constants.java
+++
@@ -1,6 +0,0 @@
-package Authentication;
-
-public class Constants {
-    final static String USER_PROFILE_TXT = "src/main/java/Authentication/UserProfile.txt";
-    final static String delimiter = "<!>";
-}
